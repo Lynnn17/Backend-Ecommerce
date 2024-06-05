fb2e5a971c503d556de1ebf40580a65f33d4441b
Lynt <lintangkusuma17@gmail.com>
2024-06-05 15:56:43 +0700

    Bug fix and Refresh Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./config/Database.js";
 import Users from "./models/UserModel.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ try {
 } catch (error) {
   console.error(error);
 }
+app.use(cookieParser());
 app.use(express.json());
 app.use(router); //middleware
 app.listen(5000, () => console.log("Server running at port 5000"));
